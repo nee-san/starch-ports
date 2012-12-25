6145842e62119db6d9034bd96a951f3a443cfb5f
strake <strake888@gmail.com>
2012-12-25 12:26:01 -0500

    core/coreutils/fgrep

--- core/coreutils/fgrep.sh
+++ core/coreutils/fgrep.sh
@@ -1,5 +1,7 @@
 #!/bin/ksh
 
+GREPFLAGS='';
+
 xf=$(mktemp) || exit;
 
 have_x="";
@@ -23,11 +25,12 @@ function qre {
 	' | sed 's/.$//'
 }
 
-while getopts "e:f:x" o; do
+while getopts ":e:f:x" o; do
 	case $o in
 		(e)	echo -n "$OPTARG" | qre >$xf; have_x=yes;;
 		(f)	cat     "$OPTARG" | qre >$xf; have_x=yes;;
 		(x)	xFlag=yes;;
+		(\?)	GREPFLAGS="$GREPFLAGS -$OPTARG"
 	esac
 done
 
@@ -35,4 +38,4 @@ shift $(dc -e "$OPTIND 1 - p");
 
 if test -z $have_x; then echo -n "$1" | qre >$xf; shift; fi
 
-grep -E "$(if test -n "$xFlag"; then sed 's/^.*$/^&$/'; else cat; fi <$xf)" "$@"
+grep $GREPFLAGS -E "$(if test -n "$xFlag"; then sed 's/^.*$/^&$/'; else cat; fi <$xf)" "$@"
